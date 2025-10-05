cf82a6b6ec991a0fe45eafb232aad07479041999
Mohamed Aatik <moaatik@e3r8p18.1337.ma>
2025-10-05 09:32:56 +0100

    Update

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -5,9 +5,10 @@ int	main(int argc, char **argv)
 	if (argc <= 1)
 		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl, 0);
 	for (int  i = 1; i < argc; i++)
+	{
 		for (int j = 0; argv[i][j]; j++)
 			argv[i][j] = std::toupper(argv[i][j]);
-	for (int i = 1; argv[i]; i++)
 		std::cout << argv[i];
+	}
 	std::cout << std::endl;
 }
