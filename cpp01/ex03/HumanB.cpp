49238f644b12e4012298a407f0d8e6da6f8b8121
Mohamed Aatik <moaatik@e3r10p18.1337.ma>
2025-10-07 09:37:08 +0100

    ex03

---
+++ cpp01/ex03/HumanB.cpp
@@ -0,0 +1,15 @@
+#include "HumanB.hpp"
+
+HumanB::HumanB(std::string name) : name(name), weapon(NULL) {}
+
+HumanB::~HumanB() {}
+
+void HumanB::attack()
+{
+	if (!weapon)
+		std::cout << name << " has no weapon to attack" << std::endl;
+	else
+		std::cout << name << " attacks with their " << weapon->getType() << std::endl;
+}
+
+void	HumanB::setWeapon(Weapon &weapon) { this->weapon = &weapon; }
