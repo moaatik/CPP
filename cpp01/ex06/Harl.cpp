6a88b24b9efa9f88a35d40f5e0c561c8f0bffcda
Mohamed Aatik <moaatik@e3r4p15.1337.ma>
2025-10-13 15:46:23 +0100

    better way

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -5,21 +5,25 @@ Harl::~Harl(){}
 
 void	Harl::debug()
 {
+	std::cout << "[ DEBUG ]" << std::endl;
 	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
 }
 
 void	Harl::info()
 {
+	std::cout << "[ INFO ]" << std::endl;
 	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void	Harl::warning()
 {
+	std::cout << "[ WARNING ]" << std::endl;
 	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl;
 }
 
 void	Harl::error()
 {
+	std::cout << "[ ERROR ]" << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
 
@@ -33,31 +37,12 @@ void	Harl::complain(std::string level)
 	switch (i)
 	{
 	case 0:
-		std::cout << "[ DEBUG ]" << std::endl;
 		Harl::debug();
-		std::cout << "[ INFO ]" << std::endl;
-		Harl::info();
-		std::cout << "[ WARNING ]" << std::endl;
-		Harl::warning();
-		std::cout << "[ ERROR ]" << std::endl;
-		Harl::error();
-		break;
 	case 1:
-		std::cout << "[ INFO ]" << std::endl;
 		Harl::info();
-		std::cout << "[ WARNING ]" << std::endl;
-		Harl::warning();
-		std::cout << "[ ERROR ]" << std::endl;
-		Harl::error();
-		break;
 	case 2:
-		std::cout << "[ WARNING ]" << std::endl;
 		Harl::warning();
-		std::cout << "[ ERROR ]" << std::endl;
-		Harl::error();
-		break;
 	case 3:
-		std::cout << "[ ERROR ]" << std::endl;
 		Harl::error();
 		break;
 	default:
