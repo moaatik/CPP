225dfb689afe043038abe01774e6265976776a96
Mohamed Aatik <moaatik@e3r4p15.1337.ma>
2025-10-21 21:13:23 +0100

    ex03 FragTrap

---
+++ cpp03/ex02/ClapTrap.hpp
@@ -0,0 +1,24 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap
+{
+	protected:
+		std::string	name;
+		int	hit_points;
+		int	energy_points;
+		int	attack_damage;
+	public:
+		ClapTrap();
+		ClapTrap(const std::string &name);
+		ClapTrap(const ClapTrap &copy);
+		~ClapTrap();
+		ClapTrap& operator=(const ClapTrap &copy);
+		void attack(const std::string& target);
+		void takeDamage(unsigned int amount);
+		void beRepaired(unsigned int amount);
+};
+
+#endif
